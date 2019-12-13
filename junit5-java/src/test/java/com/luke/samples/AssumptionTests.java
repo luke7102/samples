7ea6e5cc26e92f01256e3e166b23ef714377b213
Chun Kiyoung <kychun@bluehole.net>
2019-12-13 14:24:58 +0900

    assumptions post commit

---
+++ junit5-java/src/test/java/com/luke/samples/AssumptionTests.java
@@ -0,0 +1,24 @@
+package com.luke.samples;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+public class AssumptionTests {
+
+    @Test
+    @DisplayName("assumeTrue test only Linux")
+    public void testOnlyLinux() {
+        assumeTrue(System.getProperty("os.name").startsWith("Linux"), "OS is not Linux.");
+        assertEquals(2, 2);
+    }
+
+    @Test
+    @DisplayName("assumingThat test")
+    public void assumingThatTest() {
+        assumingThat(System.getProperty("os.name").startsWith("Windows"), () -> assertEquals(1, 2));
+    }
+}
